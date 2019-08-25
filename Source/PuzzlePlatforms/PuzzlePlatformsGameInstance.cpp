3d2e79746075965248caeda07859863fe13550e9
pv62 <pranav.v99@hotmail.com>
2019-08-25 21:48:49 +0800

    When To Use A GameInstance

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+
+}
