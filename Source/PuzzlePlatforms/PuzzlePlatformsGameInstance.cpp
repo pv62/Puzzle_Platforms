4bf1d01770d136ba6b3a68113bc7db20cec25ea9
pv62 <pranav.v99@hotmail.com>
2019-08-26 12:17:34 +0800

    Hosting Servers With ServerTravel

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -19,6 +19,10 @@ void UPuzzlePlatformsGameInstance::Host()
 	UEngine* Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) { return; }
 	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) { return; }
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& Address)
