245353eed48d6ddd17a98c8cd4d44ea1489d227b
pv62 <pranav.v99@hotmail.com>
2019-08-30 21:32:19 +0800

    Enabling Seamless Travel

--- Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -2,17 +2,23 @@
 
 
 #include "LobbyGameMode.h"
+#include "Engine/World.h"
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
+	Super::PostLogin(NewPlayer);
 	++NumberOfPlayers;
 	if (NumberOfPlayers >= 3)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Reached 3 Players"));
+		UWorld* World = GetWorld();
+		if (!ensure(World != nullptr)) { return; }
+		bUseSeamlessTravel = true;
+		World->ServerTravel("/Game/PuzzlePlatforms/Maps/Game?listen");
 	}
 }
 
 void ALobbyGameMode::Logout(AController* Exiting)
 {
+	Super::Logout(Exiting);
 	--NumberOfPlayers;
 }
